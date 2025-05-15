80e0120c1c34d2a1f2d0e693d1c0327c2179912a
LuisCadspm <luiscadspm@hotmail.com>
2025-05-15 16:20:09 -0300

    tudook

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-const botoes = document.querySelectorAll('.add-carrinho');
-const itensCarrinho = document.getElementById('.itens-carrinho');
-const total = document.getElementById('total');
+document.addEventListener('DOMContentLoaded', function () {
+    const botoes = document.querySelectorAll('.add-carrinho');
+    const listaCarrinho = document.getElementById('itens-carrinho');
+    const totalSpan = document.getElementById('total');
+    let total = 0;
 
-let totalpreco = 0;
+    botoes.forEach(function(botao) {
+        botao.addEventListener('click', function() {
+            const nome = botao.getAttribute('data-name');
+            const preco = Number(botao.getAttribute('data-preco'));
 
-botoes.forEach((botao) => {
-    botao.addEventListener('click', () => {
-        const nome = botao.getAttribute('data-nome');
-        const preco = parseFloat(botao.getAttribute('data-preco'));
+            const li = document.createElement('li');
+            li.textContent = `${nome} - R$${preco},00`;
+            listaCarrinho.appendChild(li);
 
-        const li = document.createElement('li');
-        li.textContent = `${nome} - R$ ${preco},00`;
-        itensCarrinho.appendChild(li);
-
-        totalpreco += preco;
-        total.textContent = totalpreco.toFixed(0);
-
-
+            total += preco;
+            totalSpan.textContent = total;
+        });
     });
 });
