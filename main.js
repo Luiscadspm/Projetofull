182cbd7f9faa06d6effb51e6d324ac46a016d5b9
LuisCadspm <luiscadspm@hotmail.com>
2025-05-16 09:35:30 -0300

    pronto

--- main.js
+++ main.js
@@ -9,8 +9,23 @@ document.addEventListener('DOMContentLoaded', function () {
             const nome = botao.getAttribute('data-name');
             const preco = Number(botao.getAttribute('data-preco'));
 
+
             const li = document.createElement('li');
             li.textContent = `${nome} - R$${preco},00`;
+
+
+            const btnExcluir = document.createElement('button');
+            btnExcluir.textContent = 'Excluir';
+            btnExcluir.className = 'btn-excluir';
+            btnExcluir.addEventListener('click', function() {
+                listaCarrinho.removeChild(li);
+                total -= preco;
+                totalSpan.textContent = total;
+            });
+            
+            li.appendChild(btnExcluir);
+            const btnAumentar = document.createElement('button');
+
             listaCarrinho.appendChild(li);
 
             total += preco;
